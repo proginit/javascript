54cae17316b42b215256b8c320465c096afc3591
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2023-12-12 23:14:12 -0400

    Definition variables

---
+++ 01-INTRO/variables.js
@@ -0,0 +1,13 @@
+// Definiciones de Variables var, let, const
+
+// solo se puede modificar si esta despues de definir el valor de la variable
+let nombre = "Alex";
+console.log(nombre);
+
+// forma antigua/ es mas de forma global para reutilizar
+var nombre2 = "Jose";
+console.log(nombre2);
+
+// Variable const // no se puede modificar el valor de la variable
+const pais = "Bolivia";
+console.log(pais);
