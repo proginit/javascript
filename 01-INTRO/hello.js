e9c6cea4c2702d8a71178c9b38e00314f0ca191a
Neiber Vasquez Carlos <neiber.vc.prog@gmail.com>
2023-12-12 23:02:47 -0400

    add folder intro

---
+++ 01-INTRO/hello.js
@@ -0,0 +1,12 @@
+/**
+ * Comentario de bloque
+ */
+
+// imprime en la consola
+console.log("Hello World!");
+
+// imprime en el documento html
+document.write("<h1>Hello World</h1>");
+
+// muestra una alerta
+alert("Hello World");
